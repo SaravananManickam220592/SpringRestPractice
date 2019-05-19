6695e7585fc10d97fb424a082e001341da3abe37
SaravananManickam2205 <sarava220592@gmail.com>
2019-05-19 15:17:12 +0530

    Changes with DB

--- src/main/java/com/example/SpringRestExample/daoimpl/ProductDaoImpl.java
+++ src/main/java/com/example/SpringRestExample/daoimpl/ProductDaoImpl.java
@@ -1,11 +1,12 @@
 package com.example.SpringRestExample.daoimpl;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.example.SpringRestExample.dao.ProductDao;
@@ -14,35 +15,40 @@ import com.example.SpringRestExample.model.Product;
 @Repository
 public class ProductDaoImpl implements ProductDao {
 
-	private static Map<Integer, Product> productsRepo = new HashMap<Integer, Product>();
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public void addProduct(Product product) {
-		productsRepo.put(product.getProductId(), product);
-	}
-	
-	@Override
-	public List<Product> getProducts(){
-		
-		Set<Integer> keys= productsRepo.keySet();
-		List<Product> productList= new ArrayList<Product>();
-		for(Integer key:keys){
-			productList.add(productsRepo.get(key));
-		}
-		
-		return productList;
-		
-	}
-	
-	@Override
-	public void updateProduct(Product product,int id) {
-		productsRepo.put(id, product);
-	}
-	
-	@Override
-	public void deleteProduct(int id) {
-		productsRepo.remove(id);
+		jdbcTemplate.execute(" INSERT INTO PRODUCTS VALUES(" + product.getProductId() + ",'" + product.getProductName()
+				+ "','" + product.getProductCategory() + "')");
 	}
 
+	@Override
+	public List<Product> getProducts() {
+
+		List<Map<String, Object>> produtsData = jdbcTemplate.queryForList("SELECT  * FROM PRODUCTS ");
+
+		List<Product> productsList = new ArrayList<Product>();
+		for (Map<String, Object> data : produtsData) {
+			Product prod = new Product(((BigDecimal)  data.get("ID")).intValue(), data.get("NAME").toString(),
+					data.get("CATEGORY").toString());
+			productsList.add(prod);
+		}
+		return productsList;
+	}
+
+	@Override
+	public void updateProduct(Product product, int id) {
+
+		jdbcTemplate.execute(" UPDATE PRODUCTS SET NAME='" + product.getProductName() + "' , CATEGORY='"
+				+ product.getProductCategory() + "' where ID=" + id + "");
+
+	}
+
+	@Override
+	public void deleteProduct(int id) {
+		jdbcTemplate.execute("DELETE FROM PRODUCTS WHERE ID=" + id + "");
+	}
 
 }
